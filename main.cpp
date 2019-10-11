7a50a1d7e906bc07ae539fbe8a57d71a75df458c
Arthur Guimaraes Serafim <arthur@dev.net.br>
2019-10-11 08:17:39 -0300

    Add final file

---
+++ main.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int main() {
+    vector <int> notes {100, 50, 20, 10, 5, 2, 1};
+    int total {0};
+    
+    cout << "How many dolars do you want to withdraw? ";
+    cin >> total;
+    
+    for (int i {0}; i < 7; i++) {
+        if (total < notes.at(i)) {
+            cout << notes.at(i) << "$: 0 " << "notes" << endl;
+        } else {
+            int modulo {0};
+            int total_notes {0};
+            int note = notes.at(i);
+            int round_note {0};
+            
+            modulo = total % note;
+            round_note = total - modulo;
+            total_notes = round_note / note;
+            total = modulo;
+            
+            cout << notes.at(i) << "$: " << total_notes << " notes" << endl;
+        }
+    }
+}
